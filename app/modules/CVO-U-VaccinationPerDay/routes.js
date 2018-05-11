71a0b3edcc517fe520e2115965382b5976f9c1d1
Justine Espin <espinjustine14@gmail.com>
2018-05-12 00:29:01 +0800

    12/05/2018 - VaccinationPerDay

--- app/modules/CVO-U-VaccinationPerDay/routes.js
+++ app/modules/CVO-U-VaccinationPerDay/routes.js
@@ -7,13 +7,36 @@ var db = require('../../lib/database')();
 
 
 router.get('/',  (req,res)=>{
-  
-	res.render('CVO-U-VaccinationPerDay/views/view.ejs');
+	db.query(`SELECT int_NumberOfVaccination FROM vaccinationperday`, (err, results, fields)=>{
+		if (err){
+			console.log(err)
+			res.redirect('/CVO_VaccinationPerDay')
+		}
+		else {
+			res.render('CVO-U-VaccinationPerDay/views/view.ejs', {results:results});
+		}
+
+	});
           
 });
 
+router.post('/', (req, res) => {
+	var vaccinationPerDay = req.sanitize(`${req.body.vaccination_number}`.trim());
 
+	if (vaccinationPerDay==""){
+		res.send("There's error");
+	}
 
-
-
+	else {
+		db.query(`UPDATE vaccinationperday SET int_NumberOfVaccination = ${vaccinationPerDay} WHERE int_VPDId = 1`, (err, results, fields)=>{
+			if (err){
+				console.log(err)
+				res.redirect('/CVO_VaccinationPerDay')
+			}
+			else {
+				res.redirect('/CVO_VaccinationPerDay')
+			}
+		});
+	}
+});
 exports.CVO_VaccinationPerDay= router;
