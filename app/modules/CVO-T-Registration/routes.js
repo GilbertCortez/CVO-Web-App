d67119d5513952727acc745d8a6be4ab0057cc6b
Gilbert Cortez <gilbert230709@gmail.com>
2018-05-16 00:03:40 +0800

    Transaction-Registration Updated
    -Image Upload
    -Pet Name Generator

--- app/modules/CVO-T-Registration/routes.js
+++ app/modules/CVO-T-Registration/routes.js
@@ -3,46 +3,35 @@ var express = require('express');
 var router = express.Router();
 var router1 = express.Router();
 var router2 = express.Router();
-var router3 = express.Router();
-var router4 = express.Router();
 var authMiddleware = require('../../core/auth');
 var db = require('../../lib/database')();
 // router.use(authMiddleware.noAuthed);
 
 
 router.get('/',  (req,res)=>{
-  
+
 	res.render('CVO-T-Registration/views/view.ejs');
-          
+
 });
 
 router1.get('/',  (req,res)=>{
-  
-	res.render('CVO-T-Registration/views/ownerregistration.ejs');
-          
+  db.query('SELECT * FROM barangay',(err,barangay)=>{
+	res.render('CVO-T-Registration/views/ownerregistration.ejs',{ba:barangay});
+});
 });
 
 router2.get('/',  (req,res)=>{
-  
-	res.render('CVO-T-Registration/views/petregistration.ejs');
-          
+  db.query('SELECT * FROM breed',(err,breed) =>{
+    db.query('SELECT * FROM colorpattern',(err,colorpattern)=>{
+        res.render('CVO-T-Registration/views/petregistration.ejs', { br: breed, co: colorpattern});
+    });
+  });
+
+
 });
 
-router3.get('/',  (req,res)=>{
-  
-	res.render('CVO-T-Registration/views/schedulevaccination.ejs');
-          
-});
-
-router4.get('/',  (req,res)=>{
-  
-	res.render('CVO-T-Registration/views/recordvaccination.ejs');
-          
-});
 
 
 exports.CVO_Registration= router;
 exports.CVO_OwnerRegistration= router1;
 exports.CVO_PetRegistration= router2;
-exports.CVO_ScheduleVaccination= router3;
-exports.CVO_RecordVaccination= router4;
